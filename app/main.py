80f2ab403b37f9bc7e554b0ae76dbd06ae4a0282
volodymyr <falod12kok-netizen@users.noreply.github.com>
2026-09-20 05:53:10 +0300

    implement linter report formatter v.3

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 import functools
 
 
 def cache(func: Callable) -> Callable:
-    cash_result = {}
+    cache_result = {}
 
     @functools.wraps(func)
-    def wrapper(*args, **kwargs) -> Callable:
-        if args in cash_result:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_result:
             print("Getting from cache")
-            return cash_result[args]
+            return cache_result[key]
         print("Calculating new result")
         result = func(*args, **kwargs)
-        cash_result[args] = result
+        cache_result[key] = result
         return result
     return wrapper
